6e069392318cd360a07fce0c09f4f249e3cc19d9
Mehmet <rabbi.cse.sust.bd@gmail.com>
2023-12-30 20:27:59 +0600

    Update basic blockchain counter app with thirdweb.

--- apps/thirdweb-app/counterapp/pages/index.tsx
+++ apps/thirdweb-app/counterapp/pages/index.tsx
@@ -1,4 +1,10 @@
-import { ConnectWallet, Web3Button, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  Web3Button,
+  useAddress,
+  useContract,
+  useContractRead,
+} from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
@@ -6,32 +12,50 @@ import { CONTRACT_ADDRESS } from "../constants/addresses";
 
 const Home: NextPage = () => {
   const address = useAddress();
-  const {contract} = useContract(CONTRACT_ADDRESS);
-  const {
-    data: counter,
-    isLoading: isCounterLoading
-  } = useContractRead(contract, "getCounter");
+  console.log(`address: ${address}`);
+  const { contract } = useContract(CONTRACT_ADDRESS);
+  const { data: counter, isLoading: isCounterLoading } = useContractRead(
+    contract,
+    "getCounter"
+  );
+
+  const { data: owner } = useContractRead(contract, "owner");
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <ConnectWallet />
         <h1>Counter Dapp</h1>
-        <div>
-          {isCounterLoading ? (
-            <p>0</p>
-          ) : (
-            <p>{counter.toNumber()}</p>
-          )}
-        </div>
+        <div>{isCounterLoading ? <p>0</p> : <p>{counter.toNumber()}</p>}</div>
         {address && (
-            <div className={styles.buttonContainer}>
-              <Web3Button 
-              contractAddress="{{CONTRACT_ADDRESS}}"
-              action={(contract) => contract.call("decrement")}>-</Web3Button>
-              <Web3Button contractAddress={CONTRACT_ADDRESS}
-              action={(contract) => contract.call("increment")}>+</Web3Button>
-            </div>
-          )}
+          <div className={styles.buttonContainer}>
+            <Web3Button
+              contractAddress={CONTRACT_ADDRESS}
+              action={(contract) => contract.call("decrement")}
+            >
+              -
+            </Web3Button>
+            <Web3Button
+              contractAddress={CONTRACT_ADDRESS}
+              action={(contract) => contract.call("increment")}
+            >
+              +
+            </Web3Button>
+          </div>
+        )}
+
+        {address == owner ? (
+          <Web3Button
+            contractAddress={CONTRACT_ADDRESS}
+            action={(contract) => contract.call("reset")}
+          >
+            Reset
+          </Web3Button>
+        ) : address ? (
+          <p>You are not the owner of the contract</p>
+        ) : (
+          <p>Connect your wallet to interact with the counter</p>
+        )}
       </div>
     </main>
   );
